25732e326f62d9499c2cfff9b3c46413dd9e91d3
cammywan <cammywan0218@gmail.com>
2017-02-27 22:00:18 -0500

    Update sketch.js

--- week3/sketch.js
+++ week3/sketch.js
@@ -33,35 +33,35 @@ function setup() {
     noStroke();
     textSize(s);
     text(x,g+n+n, e);
-    textFont(z);
+    textFont(z); //"letter "C""
     
     triangle(c,c,c+g,d,c-g,d);
     triangle(c+g,c,c+g+g,d,c,d);
     triangle(h-g,c,h+g,c,h,d);
-    triangle(j,e,j+g,f,j-g,f);
+    triangle(j,e,j+g,f,j-g,f); //grey triangles
     
     fill(pink);
     triangle(i,c,i+g,d,i-g,d);
     triangle(e-g,c,e,d,e-g-g,d);
     triangle(e,c,e+g,d,e-g,d);
     triangle(k+i-g,e,k+i+g,e,k+i,f);
-    triangle(k+i,e,k+i+g+g,e,k+i+g,f);
+    triangle(k+i,e,k+i+g+g,e,k+i+g,f); // pink triangles
     
     fill(pink);  
     noStroke();
     textSize(m);
     textFont(z);
-    text(y,k+l+m, f);
+    text(y,k+l+m, f); // letter "N"
     
     
     strokeWeight(b);
     stroke(grey);
     line(m+n,k,c-g-b,k);
     line(b+f+n+n,h-k,e,k+k-b);
-    line(b+g+n,e,p,e);
+    line(b+g+n,e,p,e); //horizonal stroke
    
     strokeWeight(b);
     stroke(pink);
-    line(q,r,q+g,r);
-    
+    line(q,r,q+g,r); 
+    // vertical stroke
 }
