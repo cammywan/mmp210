d5790b944038128a40c73a98802742225ea66449
cammywan <cammywan0218@gmail.com>
2017-03-01 12:58:13 -0500

    Add files via upload

---
+++ week4/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(640,480);   
+}
+
+function draw() {
+    var start=255;
+    var zero=0;
+    var blue=color("#123456");
+    var distance=50;
+    var size=30;
+	var time=frameCount%255;//variables
+	
+    background(blue);
+    
+    noStroke();
+    fill(time,start,start);
+    ellipse(mouseX,mouseY+distance,size,size);
+       
+    fill(start,time,start);
+    ellipse(mouseX-distance,mouseY,size,size);
+       
+    fill(start,start, time);
+    ellipse(mouseX+distance,mouseY,size,size);   //three dots
+}
