b8721df4267587430a15c698f4c39eaf14b8fc8f
cammywan <cammywan0218@gmail.com>
2017-03-22 00:45:38 -0400

    Update sketch.js

--- week6/sketch.js
+++ week6/sketch.js
@@ -9,25 +9,16 @@ function draw() {
 	var x=10;
 	var y=10;
 	var r,g,b;//variables
-	  background(green);
-    if (frameCount % 40 == 0) {
-      r = random(0,255);
-			g = random(0,100);
-      b = random(100,255);
+	background(green);
+        if (frameCount % 40 == 0) {
+        r = random(0,255);
+	g = random(0,100);
+        b = random(100,255);
 }	
-	textSize(20);
-        text("Cammy", x, y);    
-    x += 20;
-    if (x > width) {
-        x = 0;
-    }
-    if (x > width/2) {
-        y-=20;
-    } else {
-        y+=20;
-    }
-
-	  translate(mouseX, mouseY);
+        ellipse(mouseX-150,mouseY, x+frameCount%50,x+frameCount%50);
+	ellipse(mouseX+150,mouseY, x+frameCount%50,x+frameCount%50);
+    
+    translate(mouseX, mouseY);
     noStroke();
     fill(r,g,b);
 	  rotate(PI*frameCount/120);
