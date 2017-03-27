7a311e8d09fb230a8cc5a57bfaa233dd7b9eb0e4
cammywan <cammywan0218@gmail.com>
2017-03-27 12:07:33 -0400

    Update sketch.js

--- week6/sketch.js
+++ week6/sketch.js
@@ -14,9 +14,9 @@ function draw() {
         r = random(0,255);
 	g = random(0,100);
         b = random(100,255);
-}	
+}// color change	
         ellipse(mouseX-150,mouseY, x+frameCount%50,x+frameCount%50);
-	ellipse(mouseX+150,mouseY, x+frameCount%50,x+frameCount%50);
+	ellipse(mouseX+150,mouseY, x+frameCount%50,x+frameCount%50);// two circles that change size
     
     translate(mouseX, mouseY);
     noStroke();
@@ -26,6 +26,6 @@ function draw() {
     ellipse(0,0+distance,size,size);
     ellipse(0-distance,0,size,size);
     ellipse(0+distance,0,size,size);
-    ellipse(0,0-distance,size,size);
+    ellipse(0,0-distance,size,size); // four circles that rotate
 
 }
