91c1f49672de7a02a32d22f5c695847504d75d32
cammywan <cammywan0218@gmail.com>
2017-04-09 21:54:18 -0400

    Update midterm.js

--- midterm/midterm.js
+++ midterm/midterm.js
@@ -3,7 +3,7 @@ function setup() {
     background("white");
     
     //variables
-		noStroke();
+    noStroke();
     
     var numberOfRectangles = 400;
     var columns = Math.sqrt(numberOfRectangles);
@@ -12,46 +12,40 @@ function setup() {
     var waveSize;
     var cloudS;//small circle of clouds size
     var cloudB;//big cicle of clouds size
-	  var w = 150;
+    var w = 150;
     var h = 75;
 
   
-	   for (var x = 0; x <= width; x += w) {
+    for (var x = 0; x <= width; x += w) {
         for (var y = 0; y <= height; y += h) {
             var r,g,b;
-            var tempX = x;
-            var tempW = w;
-            var tempH = h;
 
-        if (y < height/3) {
-        // random cloud blue color
-        r = random(0, 50);
-        g = random(150, 255);
-        b = random(200, 255);
-        //random cloud size and position
-        cloudS= random(15, 60);
-        cloudB=1.8*cloudS;
-        } else if (y > height * 2/3) {
-        // random sand color
-        r = random(200, 250);
-        g = r;
-        b = random(75,150);
-
-        } else {
-        // random ocean color
-        r = random(0, 100);
-        g = random(0, 150);
-        b = random(150, 255);
-
-        // random ocean size
-            waveSize= random(0,100);
-        }
+       	    if (y < height/3) {
+           // random cloud blue color
+           r = random(0, 50);
+           g = random(150, 255);
+           b = random(200, 255);
+           //random cloud size and position
+           cloudS= random(15, 60);
+           cloudB=1.8*cloudS;
+           } else if (y > height * 2/3) {
+           // random sand color
+           r = random(200, 250);
+           g = r;
+           b = random(75,150);
+	   } else {
+          // random ocean color
+           r = random(0, 100);
+           g = random(0, 150);
+           b = random(150, 255);
+          // random ocean size
+           waveSize= random(0,100);
+           }
 
             
-            fill(r,g, b);// color fills            
+           fill(r,g, b);// color fills            
 
-            
-            if ( y <height * 1/3) {
+           if ( y <height * 1/3) {
                 // clouds
                 noStroke();
                 ellipse(x, y, cloudB, cloudB);
@@ -70,10 +64,8 @@ function setup() {
                 strokeWeight(3);
                 for (i=0;i<=120;i+=15){
                     arc(x,y,waveSize+i,waveSize+i,PI,0);
-                }
-
-				}
-		 }
-    
-}
+                		}
+			}
+		}   
+	}
 }
